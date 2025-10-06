971080edd5e5cdcdd654c4d825a01d23f303e92f
Omkar12218549 <121383250+Omkar12218549@users.noreply.github.com>
2025-10-06 15:50:21 +0000

    om

---
+++ .devcontainer/01BASICS/03conversionOperation.js
@@ -0,0 +1,24 @@
+let score="33abc";
+// console.log(typeof score);
+// console.log(typeof(score));//"33"
+
+
+
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//NaN=>not a number
+
+let isloggedin="undefined";
+let booleanislogged=Boolean(isloggedin);
+// console.log(typeof booleanislogged);
+// console.log(booleanislogged);//true
+
+// 1=>true;;0=>false
+// ""=>false;;"omkar"=>true
+// null=>true;;" "=>true
+// undefined=>true;;"0"=>true ""=>false
+
+let someNumber="omkar";
+let stringNumber=String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);//"33"
