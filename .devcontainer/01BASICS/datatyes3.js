99a84936c4e891a12ad3ebac9dd17f123fbe756a
Omkar12218549 <121383250+Omkar12218549@users.noreply.github.com>
2025-10-06 05:19:32 +0000

    commit

---
+++ .devcontainer/01BASICS/datatyes3.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version of code
+//alert("hello omkar"). The error occurs because alert is not available in Node.js (it's a browser function). To fix the error, replace
+
+console.log(8+9+"om"+23+12); console.log("hello omkar");//code readibility is violeted
+
+
+//primitives datatypes
+//number=>2 to power 53
+//bigint=>more than 2 to power 53
+//string=>'' "" ``
+//boolean=>true/false
+//null=>standalone value
+//undefined=>value is not assigned
+//object=>collection of key value pair
+//symbol=>unique
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
---
+++ .devcontainer/01BASICS/variables.js
@@ -0,0 +1,21 @@
+const accountid = 14107
+let accountEmail="omkar@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+// var has scope problem
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+let has block scope problem
+because of issue in loop and if else block
+
+const has no scope problem
+*/
+//accountid=2
+let accounstate;
+accountEmail="om@.com"
+accountPassword="54321"
+accountCity="Delhi"
+
+console.log(accountid);
+console.table([accountid,accountEmail,accountPassword,accountCity,accounstate])
