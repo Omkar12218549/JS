4647a7ccc3af437a7b7a5118d5c26088eed05e57
Omkar12218549 <121383250+Omkar12218549@users.noreply.github.com>
2025-10-06 16:34:54 +0000

    commit

--- .devcontainer/01BASICS/03conversionOperation.js
+++ .devcontainer/01BASICS/03conversionOperation.js
@@ -20,5 +20,52 @@ let booleanislogged=Boolean(isloggedin);
 
 let someNumber="omkar";
 let stringNumber=String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);//"33"
+// console.log(typeof stringNumber);
+// console.log(stringNumber);//"33"
+
+
+
+//*********Operations*********** */
+
+let val=3;
+
+let negval=-val;
+// console.log(negval);//-3
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%6);
+
+let str1="hello";
+let str2="world";
+
+let str3=str1+" "+str2;
+// console.log(str3);//hello world
+
+
+// console.log("1"+1);//11
+// console.log(1+"1");//11
+// console.log("1"+1+1);//111
+// console.log(1+1+"1");//21
+// console.log("1"+1+1+"1");//1111
+// console.log(3+4*5%3);//5
+
+// console.log(+true);//1
+// console.log(+"")//0
+
+// let gamePoint=100;
+// gamePoint++;
+// console.log(gamePoint);//101
+
+// let x=100;
+// let y=x++;
+// console.log(x);//101
+// console.log(y);//100
+
+// let a=1;
+// let b=a--;
+// console.log(a,b);//0 1
+
+
