d2c1f27a19b8b2a1dab32cd0fc3f782cd38d22a1
Omkar12218549 <121383250+Omkar12218549@users.noreply.github.com>
2025-10-12 13:58:49 +0000

    commit

---
+++ .devcontainer/01BASICS/04comparison.js
@@ -0,0 +1,25 @@
+//comparison operator in javascript
+let a=10;
+let b=10;
+
+// console.log(a==b);//true
+// console.log(a!=b);//false
+
+// console.log(a>b);//false
+// console.log(a===b);//true
+
+
+// console.log("10"==10)//true
+
+// ========. Strict checking of datatype and value
+// console.log("10"===10)//false
+
+// console.log(null==undefined);//true
+
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null==0);
+
+console.log(undefined>=0);
+console.log(undefined<0);
+console.log(undefined==0);
