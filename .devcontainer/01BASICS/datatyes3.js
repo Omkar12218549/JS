f43f0ff179399d045f57766f8a476328198b14ed
Omkar12218549 <121383250+Omkar12218549@users.noreply.github.com>
2025-12-04 06:21:45 +0000

    memory in js

--- .devcontainer/01BASICS/datatyes3.js
+++ .devcontainer/01BASICS/datatyes3.js
@@ -15,9 +15,56 @@ console.log(8+9+"om"+23+12);
 //object=>collection of key value pair
 //symbol=>unique
 
-console.log(typeof Boolean)
-console.log(typeof null);//object
-console.log(typeof undefined);//undefined
-console.log(typeof Symbol);
+// console.log(typeof Boolean)
+// console.log(typeof null);//object
+// console.log(typeof undefined);//undefined
+// console.log(typeof Symbol);
 
 
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id === anotherid)
+
+
+//non-primitve (reference type) Array,Object,Function
+let superHero=["IronMAn","Hulk","Wanda"]
+
+let myObj= {
+    name:"omkar",
+    surname:"Yadav",
+    status:"Nothing",
+
+}
+
+const myFun = function(){
+    console.log("This is my first js function");
+}
+
+let ans=myFun();
+// console.log(myObj)
+// console.log(ans)
+// console.log(superHero)
+
+// ++++++++++++++++. Memory in JS
+//  Primitive(Call by value)_______---------Use STACK. (main value does not change)
+//  Non-Primitive(Call by reference)_______---------Use HEAP. 
+let myName="OMkar YAdav"
+let anotherNAme=myName
+
+anotherNAme="AkashYAdav"
+console.log(myName)//Omakr
+console.log(anotherNAme)// Akash.  here value of main var  does not  change 
+ 
+
+ // call by reference in HEAP
+let userOne={
+    email:"userone@gmail.com",
+    upi:"userOneUPI"
+
+}
+let userTwo=userOne
+userTwo.email="user@gmail.com"
+userTwo.upi="user2UPI"
+
+console.log(userOne)
+console.log(userTwo)
