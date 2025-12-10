3cd5d7137463f1e0b5c0bbf10115713126e13121
Omkar12218549 <121383250+Omkar12218549@users.noreply.github.com>
2025-12-10 05:19:20 +0000

    Arrowow

--- .devcontainer/03_BASICS/03_Arrowfun.js
+++ .devcontainer/03_BASICS/03_Arrowfun.js
@@ -41,4 +41,23 @@ const chai=()=>{
     console.log(this.username)//undefined
     console.log(this)//{}
 }
-chai()
+// chai()
+
+//()=>{}
+
+    // const addtwoNUm=(num1,num2)=>{
+    //     return num1+num2
+
+    // }
+    //   console.log(addtwoNUm(34,43))//77
+
+ //arrow function using impicit returnn 
+    //  const addtwoNUm=(num1,num2)=> num1+num2//both are the same 
+    // const addtwoNUm=(num1,num2)=> (num1+num2)//both are the same
+    // console.log(addtwoNUm(34,43))//77
+
+    const addtwoNUm=(num1,num2)=> ({username:"omkar"})
+    console.log(addtwoNUm(22,33))
+    const objinFun=(num1,num2)=>({username:"AKKA"})
+    console.log(objinFun(33,43))
+    
